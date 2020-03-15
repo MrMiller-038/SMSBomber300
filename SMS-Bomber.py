62983d573ab016f652c18207d869354acd48c873
Ivan-Hacker-700 <62137835+Ivan-Hacker-700@users.noreply.github.com>
2020-03-15 17:09:46 +0300

    Update SMS-Bomber.py

--- SMS-Bomber.py
+++ SMS-Bomber.py
@@ -3,7 +3,8 @@
 
 import os
 import time
-import requests, random, datetime, sys, time, argparse, os , colorama
+import , random, datetime, sys, time, argparse, os , colorama
+import requests
 from colorama import Fore, Back, Style
 from colorama import init
 init()
